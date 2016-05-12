6e7861a591f5f3a946c3339c5adaf9043c734b8d
Pietro Di Bello <pierodibello@betelgeuse.station>
2016-05-12 17:37:36 +0200

    first commit with a simple helloworld

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+/*TODO:
+ * project layout
+ * maven
+ * packages
+ * keywords sconosciute public, class, static....
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.err.println("Hello world");
+    }
+}
