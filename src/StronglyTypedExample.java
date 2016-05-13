6f036fb41559c31771ede0d010ca68416dd7bfa2
Pietro Di Bello <pierodibello@betelgeuse.homenet.telecomitalia.it>
2016-05-13 11:52:07 +0200

    add strongly-statically-typed example

---
+++ src/StronglyTypedExample.java
@@ -0,0 +1,11 @@
+
+public class StronglyTypedExample {
+
+    public static void main(String[] args) {
+        // name = "Piero"; // Java is statically typed!
+        String name = "piero"; // OK...
+
+        // name = 12;// Java is strongly-typed!
+        name = "Paolo"; // OK
+    }
+}
