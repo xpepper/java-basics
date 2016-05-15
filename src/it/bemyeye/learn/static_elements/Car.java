68d66978e21c61f54b823f618d5152c95227d07c
Pietro Di Bello <pierodibello@betelgeuse.station>
2016-05-15 21:55:03 +0200

    add example on static fields and methods

---
+++ src/it/bemyeye/learn/static_elements/Car.java
@@ -0,0 +1,29 @@
+package it.bemyeye.learn.static_elements;
+
+public class Car {
+    private static final int DEFAULT_ENGINE_CYLINDERS = 1000;
+    private static double BASIC_BOLLO_TAX_RATE = 0.75;
+
+    int cylinders;
+    int currentSpeed;
+
+    public static Car createWithStartingSpeed(int startingSpeed) {
+        Car car = new Car();
+        car.currentSpeed = startingSpeed;
+        return car;
+    }
+
+    public Car() {
+        this(DEFAULT_ENGINE_CYLINDERS);
+    }
+
+    public Car(int engineDisplacement) {
+        this.cylinders = engineDisplacement;
+    }
+
+    double calcolaBollo() {
+        return cylinders * BASIC_BOLLO_TAX_RATE;
+    }
+
+}
+
