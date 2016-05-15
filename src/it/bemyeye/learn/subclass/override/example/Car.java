ebe6b4e0cfe6e4e80df943933a85577777bcb552
Pietro Di Bello <pierodibello@betelgeuse.station>
2016-05-15 15:11:54 +0200

    add example of ovverriding and calling super on a overridden method

---
+++ src/it/bemyeye/learn/subclass/override/example/Car.java
@@ -0,0 +1,20 @@
+package it.bemyeye.learn.subclass.override.example;
+
+class Car {
+    int cylinders;
+    int currentSpeed;
+
+    public Car(int cylinders) {
+        this.cylinders = cylinders;
+        this.currentSpeed = 0;
+    }
+
+    void accelerate(int speedGain) {
+        currentSpeed += speedGain;
+    }
+
+    public Double calcolaBollo() {
+        return cylinders * 0.75;
+    }
+
+}
